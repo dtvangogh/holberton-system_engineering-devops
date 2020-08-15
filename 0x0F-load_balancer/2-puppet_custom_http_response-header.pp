beb1fa2f48622bd64b2a9f5704d108a349cf0e08
dt <dtvangogh@github.com>
2020-08-15 00:00:32 +0000

    first attempt at puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,20 @@
+# puppet manifest creating a custom HTTP header response
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By $hostname;'
+}
+
+file { '/usr/share/nginx/html/index.html':
+  content => 'Holberton School',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
