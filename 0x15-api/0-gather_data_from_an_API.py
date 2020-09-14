5415bd4a43d5ddff29369d8bd59a7cced48b2aaa
dtvangogh <dt.van16@gmail.com>
2020-09-14 16:43:42 -0400

    pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-fetches from https://jsonplaceholder.typicode.com and pushes  information about the employee todo list. takes userId of employee as argument
+fetches from https://jsonplaceholder.typicode.com and pushes
+information about the employee todo list. takes userId of employee as argument
 """
 
 import requests
@@ -10,8 +11,9 @@ if __name__ == '__main__':
     userId = argv[1]
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
                         format(userId), verify=False).json()
-    todo_list = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(userId), verify=False).json()
+    todo_list = requests.get("https://jsonplaceholder.typicode.com/
+                             todos?userId={}".format(userId),
+                             verify=False).json()
     number_of_done_tasks = []
     for task in todo_list:
         if task.get('completed') is True:
@@ -19,4 +21,5 @@ if __name__ == '__main__':
     print("Employee {} is done with tasks({}/{}):".
           format(user.get('name'), len(number_of_done_tasks), len(todo_list)))
     separator = "\n"
-    print(separator.join("\t {}".format(task) for task in number_of_done_tasks))
+    print(separator.join("\t {}".format(task)
+          for task in number_of_done_tasks))
