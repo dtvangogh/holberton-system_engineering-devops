70e4b888c46463d3d2d14bf0781566fb34a238ff
dtvangogh <dt.van16@gmail.com>
2020-09-14 15:32:21 -0400

    number 1 completed

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+fetches from https://jsonplaceholder.typicode.com and pushes  information about the employee todo list. takes userId of employee as argument
+"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    userId = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(userId), verify=False).json()
+    todo_list = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(userId), verify=False).json()
+    number_of_done_tasks = []
+    for task in todo_list:
+        if task.get('completed') is True:
+            number_of_done_tasks.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(number_of_done_tasks), len(todo_list)))
+    separator = "\n"
+    print(separator.join("\t {}".format(task) for task in number_of_done_tasks))
