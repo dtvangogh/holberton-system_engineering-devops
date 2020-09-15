15605038eeb3f4ca652e2e42823e0962dbb68dfe
dtvangogh <dt.van16@gmail.com>
2020-09-14 20:49:21 -0400

    add

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,2 +1,29 @@
 #!/usr/bin/python3
-no prototype
+"""
+export data to json
+"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """
+    Records all tasks that are owned by this employee
+    """
+    employees = requests.get("https://jsonplaceholder.typicode.com/users").json()
+    json_data = {}
+    for employee in employees:
+        employee_data = []
+        userId = employee['id']
+        tasks = requests.get("https://jsonplaceholder.typicode.com/"
+                                 "todos?userId={}".format(userId)).json()
+        for task in tasks:
+            task_data = {}
+            task_data["task"] = task.get('title')
+            task_data["completed"] = task.get("completed")
+            task_data["username"] = employee['username']
+            employee_data.append(task_data)
+        json_data[userId] = employee_data
+    with open("todo_all_employees.json", 'w') as jsonfile:
+        json.dump(json_data, jsonfile)
