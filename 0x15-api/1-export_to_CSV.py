030dbdc4997ecacd947917237b6881ac7916c209
dtvangogh <dt.van16@gmail.com>
2020-09-18 23:46:47 -0400

    pep8 :(

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-fetches from https://jsonplaceholder.typicode.com and pushes  information about the employee todo list. takes userId of employee as argument
+fetches from https://jsonplaceholder.typicode.com
 """
 
 import requests
@@ -10,9 +10,9 @@ import csv
 if __name__ == '__main__':
     userId = int(argv[1])
     user_name = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                            .format(userId)).json().get("username")
+                             .format(userId)).json().get("username")
     tasks = requests.get("https://jsonplaceholder.typicode.com/"
-                             "todos?userId={}".format(userId)).json()
+                         "todos?userId={}".format(userId)).json()
     with open("{}.csv".format(userId), "w") as cvsfile:
         write = csv.writer(cvsfile, quoting=csv.QUOTE_ALL)
         for task in tasks:
