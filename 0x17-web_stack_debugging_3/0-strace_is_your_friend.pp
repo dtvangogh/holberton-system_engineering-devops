2f17409a1fdfb98132599e332764ec62963e57e5
dtvangogh <dt.van16@gmail.com>
2020-09-30 22:52:42 -0400

    add

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# fixes the holberton container using apuppet script
+# Fixes the holberton container using apuppet script
 $fix = "/usr/bin/env sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php"
 
 exec { 'fix line 137':
