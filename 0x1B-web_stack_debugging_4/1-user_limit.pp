01d5cc25967cee96ca9b86792cfe8eceef4a5192
dt <dtvangogh@github.com>
2020-10-09 01:37:11 +0000

    finished

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+# fixes the server
+$script = "/usr/bin/env sed -i 's/nofile [0-9]/nofile 10240/g' /etc/security/limits.conf"
+
+exec { 'Change OS config to allow login with Holberton user':
+  command => $script
+} 
