85886a362dc4aebd6c48096249147e5418bc9842
dt <dtvangogh@github.com>
2020-10-09 01:30:52 +0000

    add

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,5 +2,5 @@
 $script = "/usr/bin/env sed -i 's/15/4096/g' /etc/default/nginx && /usr/sbin/service nginx restart"
 
 exec { 'Fix nginx worker limit':
-  command => $script 
+  command => $script
 }
